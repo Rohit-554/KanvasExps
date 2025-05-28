632645e9594e5873be2847cf61382bbea2c908f8
Jadu <48874687+Rohit-554@users.noreply.github.com>
2025-05-28 20:37:13 +0530

    PulsingCircle

---
+++ app/src/main/java/io/jadu/kanvasexp/burpy/PulsingCircle.kt
@@ -0,0 +1,42 @@
+package io.jadu.kanvasexp.burpy
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+@Composable
+fun PulsingCircle() {
+    // Infinite animation setup
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.3f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .scale(scale)
+                .background(Color.Cyan, shape = CircleShape)
+        )
+    }
+}
